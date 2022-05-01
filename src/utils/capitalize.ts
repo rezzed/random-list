d9cea5a1f2cee3c6e9a4ac56551ba7db22749ea2
JamesBoon <JamesBoon@users.noreply.github.com>
2022-05-01 14:20:32 +0200

    Removed unused capitalize and toPascalCase utils

--- src/utils/capitalize.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Capitalize a string e.g. "FOO" -> "Foo"
- */
-export function capitalize(value: string) {
-  return value ? value.charAt(0).toUpperCase() + value.slice(1).toLowerCase() : '';
-}
